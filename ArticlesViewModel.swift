c7304aa558676575239409d8fe7f02bcf8a07ff1
iam-that-one <iam-that-one@users.noreply.github.com>
2021-04-04 05:12:20 +0300

    Date formatting

--- ArticlesViewModel.swift
+++ ArticlesViewModel.swift
@@ -73,7 +73,6 @@ struct Articles : Codable,Identifiable {
     }
     var date : Date{
         let dateFormatter = DateFormatter()
-       // dateFormatter.locale = Locale(identifier: "en_US_POSIX") // set locale to reliable US_POSIX
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         let date = dateFormatter.date(from:publishedAt)!
         return date
